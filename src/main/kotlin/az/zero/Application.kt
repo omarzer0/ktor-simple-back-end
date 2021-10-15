8d552a94f91c2b4ce4e657db890cada120199488
omarzer0 <omarzero1015@gmail.com>
2021-10-15 20:08:42 +0200

    init project

---
+++ src/main/kotlin/az/zero/Application.kt
@@ -0,0 +1,14 @@
+package az.zero
+
+import io.ktor.application.*
+import az.zero.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureMonitoring()
+    configureRouting()
+    configureSerialization()
+}
